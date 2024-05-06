a2e2c9565bda43d0c9098126281f120fe9270dbc
Toni Liungan <71494942+toniliu672@users.noreply.github.com>
2024-05-07 00:26:05 +0800

    Create 2488.py

---
+++ 2488.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def countSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        from collections import defaultdict
+
+        # Ubah array sesuai dengan petunjuk
+        for i in range(len(nums)):
+            if nums[i] < k:
+                nums[i] = -1
+            elif nums[i] > k:
+                nums[i] = 1
+            else:
+                nums[i] = 0
+
+        # Cari posisi k dalam array
+        k_pos = nums.index(0)
+
+        # Hitung prefix sum dan simpan dalam dictionary
+        prefix_sum = defaultdict(int)
+        prefix_sum[0] = 1
+        sum = 0
+        for num in nums[k_pos+1:]:
+            sum += num
+            prefix_sum[sum] += 1
+
+        # Hitung jumlah subarray dengan median k
+        res = prefix_sum[0] + prefix_sum[1]
+        sum = 0
+        for num in reversed(nums[:k_pos]):
+            sum += num
+            res += prefix_sum[-sum] + prefix_sum[1-sum]
+
+        return res
